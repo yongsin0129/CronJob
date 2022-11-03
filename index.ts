5fe78d0945ab7951f4efee3dc222a54fb509cca9
yongsin0129 <yongsin0129@gmail.com>
2022-11-03 21:12:35 +0800

    setInterval test

--- index.ts
+++ index.ts
@@ -7,12 +7,18 @@ const app = express()
 const port = process.env.PORT
 
 const word = 'It`s a test word'
-const time = Date.now()
+const time = new Date()
+let number = 0
+
+const timerNumber = setInterval(() => {
+  number++
+}, 1000)
 
 app.get('/test', (req, res) => {
   res.send({
     first: 'this is a test route , check variable "time" is : ' + time,
-    second: word
+    second: word,
+    number: number
   })
 })
 
