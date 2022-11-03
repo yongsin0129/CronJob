f88263a4f852588944b523b2be471cf62d218e84
yongsin0129 <yongsin0129@gmail.com>
2022-11-04 06:39:16 +0800

    add time helper

--- index.ts
+++ index.ts
@@ -7,16 +7,16 @@ const app = express()
 const port = process.env.PORT
 
 const word = 'It`s a test word'
-const time = new Date()
-let number = 0
+const time1 = new Date().toLocaleString('zh-TW', { timeZone: 'Asia/taipei' })
+// const time1 =new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })
+const time2 = timeFormatter(new Date())
 
-// const timerNumber = setInterval(() => {
-//   addNumberAndReset()
-// }, 500)
+let number = 0
 
 app.get('/test', (req, res) => {
   res.send({
-    first: 'this is a test route , check variable "time" is : ' + time,
+    time1: 'this is a test route , Asia/Taipei Local time is : ' + time1,
+    time2: 'this is a test route , ' + time2,
     second: word,
     number: number
   })
@@ -54,4 +54,20 @@ function addNumberAndReset (): void {
   if (number >= 100) number = 0
   else number += 10
   return
+}
+
+function timeFormatter (timeObj: Date) {
+  let time = timeObj
+  let year = time.getFullYear()
+  let month = time.getMonth()
+  let date = time.getDate()
+  let day = time.getDay()
+  let hours = time.getHours()
+  let minutes = time.getMinutes()
+  let seconds = time.getSeconds()
+
+  const dayName = ['日', '一', '二', '三', '四', '五', '六']
+
+
+  return ` 現在是台北標準時間 ${year} 年 ${month + 1} 月 ${date} 日星期 ${dayName[day]} ${hours} : ${minutes} : ${seconds} `
 }
